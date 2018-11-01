b334260955973bbcf4c2f5c1cccd949f8b70a25d
Vy-Shane Xie <shane@node.mu>
2018-11-01 15:52:46 +0800

    Remove comments

--- src/main/scala/ReverseGeocoderService.scala
+++ src/main/scala/ReverseGeocoderService.scala
@@ -8,9 +8,6 @@ import mu.node.reversegeocoder.ReverseGeocoderGrpcMonix.ReverseGeocoder
 
 class ReverseGeocoderService(places: KDTreeMap[Location, Place]) extends ReverseGeocoder {
 
-  // val belmont = places.findNearest((-31.9450, 115.9270), 1)
-  // val nyc = places.findNearest((40.7128, -74.0060), 1)
-
   override def reverseGeocodeLocation(request: ReverseGeocodeLocationRequest): Task[ReverseGeocodeLocationResponse] = {
     val place = places
       .findNearest((request.latitude, request.longitude), 1)
