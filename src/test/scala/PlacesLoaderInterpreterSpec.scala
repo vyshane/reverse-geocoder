6d69eb81863f2b6c660856e8d932404c1f35bd45
Vy-Shane Xie <shane@node.mu>
2018-11-22 21:00:20 +0800

    Simplify test using Id instead of Task

--- src/test/scala/PlacesLoaderInterpreterSpec.scala
+++ src/test/scala/PlacesLoaderInterpreterSpec.scala
@@ -2,54 +2,51 @@
 
 package mu.node.reversegeocoder
 
+import cats.Id
 import com.thesamet.spatial.KDTreeMap
-import monix.eval.Task
-import monix.execution.Scheduler.Implicits.global
-import org.scalamock.scalatest.AsyncMockFactory
-import org.scalatest.{AsyncWordSpec, Matchers}
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{Matchers, WordSpec}
 
-class PlacesLoaderInterpreterSpec extends AsyncWordSpec with AsyncMockFactory with Matchers {
+class PlacesLoaderInterpreterSpec extends WordSpec with MockFactory with Matchers {
 
   "PlacesLoader" when {
     "loading an empty file" should {
       "return an empty KDTreeMap" in {
-        val fileReader = mock[FileReader[Task]]
+        val fileReader = mock[FileReader[Id]]
         val placesLoader = new PlacesLoaderInterpreter(fileReader)
 
         (fileReader.readLines _)
           .expects("path")
-          .returns(Task.now(Seq()))
+          .returns(Seq())
 
-        placesLoader.load("path").runAsync.map { k =>
-          k shouldEqual KDTreeMap.empty[Location, Place]
-        }
+        placesLoader.load("path") shouldEqual KDTreeMap.empty[Location, Place]
       }
     }
     "loading a non-empty file" should {
       "skip invalid lines and only load well-formed lines" in {
-        val fileReader = mock[FileReader[Task]]
+        val fileReader = mock[FileReader[Id]]
         val placesLoader = new PlacesLoaderInterpreter(fileReader)
 
         (fileReader.readLines _)
           .expects("path")
-          .returns(Task.now(Seq(
+          .returns(Seq(
             "100\tInvalid Line\t\t\t\t\t",
             "934131\tQuatre Bornes\tQuatre Bornes\tQuatre " +
               "Bornes\t-20.26381\t57.4791\tP\tPPL\tMU\t\t17\t\t\t\t80961\t\t335\tIndian/Mauritius\t2012-05-18"
-          )))
+          ))
 
-        placesLoader.load("path").runAsync.map { k =>
-          k.size shouldEqual 1
-          k.get((-20.26381, 57.4791)).get shouldEqual Place(
-            name = "Quatre Bornes",
-            countryCode = "MU",
-            latitude = -20.26381,
-            longitude = 57.4791,
-            elevationMeters = 0,
-            timezone = "Indian/Mauritius",
-            population = 80961
-          )
-        }
+        val places = placesLoader.load("path")
+        places.size shouldEqual 1
+
+        places.get((-20.26381, 57.4791)).get shouldEqual Place(
+          name = "Quatre Bornes",
+          countryCode = "MU",
+          latitude = -20.26381,
+          longitude = 57.4791,
+          elevationMeters = 0,
+          timezone = "Indian/Mauritius",
+          population = 80961
+        )
       }
     }
   }
